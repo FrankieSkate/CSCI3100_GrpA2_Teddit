92ad27f0098db093e28ce75c0f3ab5995e37a7c4
jimmylau <jimmylaukh@gmail.com>
2023-03-30 00:30:19 +0800

    User related api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,65 @@
 const userDAO = require("../dao/user");
 
 class UserController {
-    async userLogin(req, res){
-        try{
+
+    // async follow(req, res) {
+    //     try {
+    //         const {  } = req.body;
+    //         const ret = await userDAO;
+    //         res.status(201).json(ret);
+    //     } catch (err) {
+    //         res.status(400).send(err);
+    //     }
+    // }
+
+    async userLogin(req, res) {
+        try {
             const { useraccount, password } = req.body;
             const id = await userDAO.userLogin(useraccount, password);
-            res.status(201).send(id);
-        } catch (err){
-            res.status(400).send('err');
+            res.status(201).json(id);
+        } catch (err) {
+            res.status(400).send('error');
+        }
+    }
+
+    async follow(req, res) {
+        try {
+            const { user_id, following_id } = req.body;
+            const ret = await userDAO.follow(user_id, following_id);
+            console.log('test from user controller', ret);
+            res.status(201).json(ret);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    }
+
+    async getFollower(req, res){
+        try {
+            const { user_id } = req.query;
+            const id = Number(user_id);
+            if(isNaN(id)){
+                throw new Error('Fail to get data');
+            }
+            console.log(id,typeof(id));
+            const follower_list = await userDAO.getFollower(id);
+            console.log('test from user controller get follower', follower_list);
+            res.status(201).json(follower_list);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    }
+
+    async getFollowing(req, res) {
+        try {
+            const { user_id } = req.query;
+            const id = Number(user_id);
+            if(isNaN(id)){
+                throw new Error('Fail to get data');
+            }
+            const following_list = await userDAO.getFollowing(id);
+            res.status(201).json(following_list);
+        } catch (err) {
+            res.status(400).send(err);
         }
     }
 }
--- server/dao/user.js
+++ server/dao/user.js
@@ -1,17 +1,46 @@
-const knex = require('../data/db')
+const knex = require("../data/db")
 
 class UserDAO {
-    async userLogin(useraccount, password) {
-        const [id] = await knex('user_account')
+
+    // TODO able gmail or useraccount
+    async userLogin(useraccount, password){
+        const [id] = await knex("user_account")
+            .select("id")
             // .where(function(){
             //     // TODO: need to check with this or condition work or not
-            //     this.where('account', useraccount).orwhere('mail_address', useraccount)
+            //     this.where("account", useraccount).orwhere("mail_address", useraccount)
             // })
-            .where('account', useraccount)
-            .where('password', password)
-            .returning('id'); 
+            .where("account", useraccount)
+            .where("password", password); 
         return id;
     }
+
+    async follow(user_id, following_id){
+        const ret = await knex("user_relation")
+            .insert({
+                // TODO: need check ok or not
+                user_id, following_id
+            });
+        return ret;
+    }
+
+    async getFollower(user_id){
+        const follower_list = await knex("user_info")
+            .join("user_relation", "user_info.user_id", "=", "user_relation.user_id")
+            .where("user_relation.following_id", user_id)
+            .select("user_info.*");
+        return follower_list;
+    }
+
+    async getFollowing(user_id){
+        const following_list = await knex("user_info")
+            .join("user_relation", "user_info.user_id", "=", "user_relation.following_id")
+            .where("user_relation.user_id", user_id)
+            .select("user_info.*");
+        return following_list;
+    }
+
+
 }
 
 module.exports = new UserDAO();
