2b14e4eaef75edae0a736aacb09816147a38b312
AnthonyYeung00 <anthony3690095@gmail.com>
2023-04-10 15:22:25 +0800

    Reset password page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./scenes/loginPage";
 import HomePage from "./scenes/homePage";
+import ForgetPage from "./scenes/forgetPage";
+import ResetPage from "./scenes/resetPage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -17,6 +19,8 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/forget" element={<ForgetPage />} />
+            <Route path="/reset" element={<ResetPage />} />
             <Route path="/" element={<HomePage />} />
           </Routes>
         </ThemeProvider>
