aa0b0da744ae8424d9091cf9527bb7f77a8320a1
FrankieSkate <frankiecc9@gmail.com>
2023-04-14 03:21:10 +0800

    finished add comment

--- server/data/index.js
+++ server/data/index.js
@@ -141,11 +141,7 @@ export const posts = [
       [userIds[3], true],
       [userIds[4], true],
     ]),
-    comments: [
-      "random comment",
-      "another random comment",
-      "yet another random comment",
-    ],
+    comments: [],
   },
   {
     _id: new mongoose.Types.ObjectId(),
@@ -160,12 +156,7 @@ export const posts = [
       [userIds[1], true],
       [userIds[2], true],
     ]),
-    comments: [
-      "one more random comment",
-      "and another random comment",
-      "no more random comments",
-      "I lied, one more random comment",
-    ],
+    comments: [],
   },
   {
     _id: new mongoose.Types.ObjectId(),
@@ -180,13 +171,7 @@ export const posts = [
       [userIds[3], true],
       [userIds[5], true],
     ]),
-    comments: [
-      "one more random comment",
-      "I lied, one more random comment",
-      "I lied again, one more random comment",
-      "Why am I doing this?",
-      "I'm bored",
-    ],
+    comments: [],
   },
   {
     _id: new mongoose.Types.ObjectId(),
@@ -200,14 +185,7 @@ export const posts = [
       [userIds[6], true],
       [userIds[3], true],
     ]),
-    comments: [
-      "I lied again, one more random comment",
-      "Why am I doing this?",
-      "I'm bored",
-      "I'm still bored",
-      "All I want to do is play video games",
-      "I'm going to play video games",
-    ],
+    comments: [],
   },
   {
     _id: new mongoose.Types.ObjectId(),
@@ -222,13 +200,7 @@ export const posts = [
       [userIds[5], true],
       [userIds[7], true],
     ]),
-    comments: [
-      "I lied again, one more random comment",
-      "Why am I doing this?",
-      "Man I'm bored",
-      "What should I do?",
-      "I'm going to play video games",
-    ],
+    comments: [],
   },
   {
     _id: new mongoose.Types.ObjectId(),
@@ -242,12 +214,6 @@ export const posts = [
       [userIds[2], true],
     ]),
 
-    comments: [
-      "Can I play video games now?",
-      "No let's actually study",
-      "Never mind, I'm going to play video games",
-      "Stop it.",
-      "Michael, stop it.",
-    ],
+    comments: [],
   },
 ];
