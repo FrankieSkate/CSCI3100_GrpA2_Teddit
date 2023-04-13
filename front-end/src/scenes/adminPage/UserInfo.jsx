bb085de90f51bf1e12e3437c1e83c43def05b687
FrankieSkate <frankiecc9@gmail.com>
2023-04-14 03:39:57 +0800

    delete some useless lines and change images

--- front-end/src/scenes/adminPage/UserInfo.jsx
+++ front-end/src/scenes/adminPage/UserInfo.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Button, Box, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 
-
-const UserInfo = ({ userId, name, createdDate, email, onClick= ()=> {}, search=false }) => {
+const UserInfo = ({
+  userId,
+  name,
+  createdDate,
+  email,
+  onClick = () => {},
+  search = false,
+}) => {
   const { palette } = useTheme();
   const main = palette.neutral.main;
-  const dispatch = useDispatch();
-  const token = useSelector(state => state.token);
-  const users = useSelector(state => state.users);
-
 
   return (
     <Box
@@ -41,12 +43,14 @@ const UserInfo = ({ userId, name, createdDate, email, onClick= ()=> {}, search=f
           onClick={onClick}
           sx={{
             color: palette.background.alt,
-            backgroundColor: (search) ? palette.primary.light : palette.primary.red,
+            backgroundColor: search
+              ? palette.primary.light
+              : palette.primary.red,
             borderRadius: "0.5rem",
             "&:hover": { cursor: "pointer" },
           }}
         >
-          {search? "FOLLOW" : "DELETE"}
+          {search ? "FOLLOW" : "DELETE"}
         </Button>
       </Box>
     </Box>
--- front-end/src/scenes/searchPage/index.jsx
+++ front-end/src/scenes/searchPage/index.jsx
@@ -1,10 +1,6 @@
 import { Box } from "@mui/material";
 import Navbar from "../navbar";
 import { useNavigate } from "react-router-dom";
-import UserWidget from "../widgets/UserWidget";
-import UserPostWidget from "../widgets/UserPostWidget";
-import PostsWidget from "../widgets/PostsWidget";
-import AdvertWidget from "../widgets/AdvertWidget";
 import WidgetWrapper from "../../components/WidgetWrapper";
 import UserInfo from "../adminPage/UserInfo";
 import { useSelector, useDispatch } from "react-redux";
@@ -12,12 +8,11 @@ import { setFriends } from "../../state";
 import { Divider } from "@mui/material";
 
 const SearchPage = () => {
-  const search  = useSelector(state => state.search);
+  const search = useSelector(state => state.search);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { _id } = useSelector(state => state.user);
   const token = useSelector(state => state.token);
-
 
   const patchFriend = async () => {
     const response = await fetch(
@@ -35,47 +30,39 @@ const SearchPage = () => {
     navigate("/");
   };
 
-
-  
   return (
     <Box>
       <Navbar />
 
-
-<>
-      <WidgetWrapper>
-        <Box
-          mb="1rem"
-          width="100%"
-          display="flex"
-          justifyContent="space-between"
-          flex-direction="column"
-        >
-          <Box flexBasis="30%">{"User Id"}</Box>
-          <Box flexBasis="30%">{"Name"}</Box>
-          <Box flexBasis="30%">{"Registered Email"}</Box>
-          <Box flexBasis="30%">{"Created date"}</Box>
-          <Box flexBasis="20%">{"Action"}</Box>
-        </Box>
-        <Divider />
-        <Box mt="1rem">
-          
-              <UserInfo
-          key={search._id}
-          userId={search._id}
-          name={`${search.firstName} ${search.lastName}`}
-          email={search.email}
-          createdDate={search.createdAt}
-          onClick={patchFriend}
-          search={true}
-          />
-           
-        </Box>
-      </WidgetWrapper>
-      
-    </>
-
-
+      <>
+        <WidgetWrapper>
+          <Box
+            mb="1rem"
+            width="100%"
+            display="flex"
+            justifyContent="space-between"
+            flex-direction="column"
+          >
+            <Box flexBasis="30%">{"User Id"}</Box>
+            <Box flexBasis="30%">{"Name"}</Box>
+            <Box flexBasis="30%">{"Registered Email"}</Box>
+            <Box flexBasis="30%">{"Created date"}</Box>
+            <Box flexBasis="20%">{"Action"}</Box>
+          </Box>
+          <Divider />
+          <Box mt="1rem">
+            <UserInfo
+              key={search._id}
+              userId={search._id}
+              name={`${search.firstName} ${search.lastName}`}
+              email={search.email}
+              createdDate={search.createdAt}
+              onClick={patchFriend}
+              search={true}
+            />
+          </Box>
+        </WidgetWrapper>
+      </>
     </Box>
   );
 };
